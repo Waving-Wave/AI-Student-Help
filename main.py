a54bb2840979967ab09a79dd6f6889e2cd413c86
EB-OP <elliot.bolter@gmail.com>
2022-07-07 09:11:25 +1200

    Comments

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ from torch import true_divide
 
 #Create a file storage for AI presets
 
+
+
+
+
+
+#Gets the user's basic parameters for text completion/generation.
 promptLength = int(input("How long you would like your response? (In character length; 1-4000) "))
 promptRandomness = float(("0." + input("How random would you like your prompt? (1-10) ")))
 promptMessage = input("Prompt: ")
 
+#Sets my OpenAI unique key, account reqiured to submit prompts
 openai.api_key = "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"
 
+#Send the pre-set information to the OpenAI server to return the text completion
 response = openai.Completion.create(
   model="text-davinci-002",
   prompt=promptMessage,
@@ -21,4 +29,5 @@ response = openai.Completion.create(
   presence_penalty=0
 )
 
+#This eliminates all the outside information that is given as a response by the OpenAI completion
 print(response["choices"][0]["text"])
